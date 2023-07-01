807e713efd59a3ab6338e306d41ddf6667ce6b58
miku1001 <pauloted.22@gmail.com>
2023-07-02 00:36:33 +0800

    Test Drive for PetClass

--- Pet Class/PetTest.py
+++ Pet Class/PetTest.py
@@ -6,9 +6,19 @@ from termcolor import colored
 class TestPet:
     def test(self):
 
+        name = input(colored(("Enter the name of pet: "), color = "green"))
+        animal_type = input(colored(("Enter the type of animal:"), color = "blue"))
+        age = input(colored(("Enter the age(in years): "), color = "yellow"))
+
+        pet = Pet(name, animal_type, age)
 
 
-
+        # print pet details
+        print(colored("=" * 72), color = "red")
+        print()
+        print(f'His/Her name is \033[32m{pet.get_name()}\033[0m')
+        print(f'He/She is a \033[34m{pet.get_animal_type()}\033[0m')
+        print(f'He/She is \033[33m{pet.get_age()} years old\033[0m')
 
 
 
