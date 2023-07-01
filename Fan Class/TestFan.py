1bb9f0d1676bf13c26c16257a021be42aa0a56c8
miku1001 <pauloted.22@gmail.com>
2023-07-01 20:17:39 +0800

    Creating Test Class(2)

--- Fan Class/TestFan.py
+++ Fan Class/TestFan.py
@@ -5,6 +5,11 @@ from termcolor import colored
 # Create a Test Driver program named FanClass
 class TestTV:
     def test(self):
-        print("\033[1;31;40m Fan Status \033[0m".center(80))
-        print(colored(("-" * 72), color='red'))
+        print("\032[1;31;40m Fan1 Status \033[0m".center(80))
+        print(colored(("-" * 72), color='green'))
+
+
+
+        print("\032[1;31;40m Fan2 Status \033[0m".center(80))
+        print(colored(("-" * 72), color='green'))
     
