d81bacab0451c54fc8f6add0d257a95dafd9f677
miku1001 <pauloted.22@gmail.com>
2023-07-01 23:53:56 +0800

    Create Test Drive for ClassPet

---
+++ Pet Class/PetTest.py
@@ -0,0 +1,17 @@
+# Import FanClass
+from PetClass import Pet
+from termcolor import colored
+
+# Create a Test Driver program named ClassPet
+class TestPet:
+    def test(self):
+
+
+
+
+
+
+
+# __start__
+test_run = TestPet()
+test_run.test()
