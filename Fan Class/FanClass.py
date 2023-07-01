6014b7820f58a145b7e0d1b398c72fe66979f607
miku1001 <pauloted.22@gmail.com>
2023-07-01 17:16:51 +0800

    Getter and setter for color and radius

--- Fan Class/FanClass.py
+++ Fan Class/FanClass.py
@@ -14,18 +14,34 @@ class Fan:
         self.__radius = radius
         self.__color = color
 
-    # Create getter methods for fan speed
+    # Create getter method for fan speed
     def get_fan_speed(self):
         return self.__speed
         
-    # Create setter methods for fan speed
+    # Create setter method for fan speed
     def set_fan_speed(self, speed):
         self.__speed = speed
     
-    # Create getter methods for power status
+    # Create getter method for power status
     def get_on(self):
         return self.__on
         
-    # Create setter methods for fan speed
+    # Create setter method for power status
     def set_on(self, on):
         self.__on = on
+    
+    # Create getter method for radius
+    def get_radius(self):
+        return self.__radius
+        
+    # Create setter method for radius
+    def set_on(self, radius):
+        self.__radius = radius
+    
+    # Create getter method for color
+    def get_radius(self):
+        return self.__color
+        
+    # Create setter method for radius
+    def set_on(self, color):
+        self.__color = color
