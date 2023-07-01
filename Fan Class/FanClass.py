8c98e438c163c4a4f001dbf4d07edb0fa5a09f61
miku1001 <pauloted.22@gmail.com>
2023-07-01 17:12:12 +0800

    Setter and getter for fan speed

--- Fan Class/FanClass.py
+++ Fan Class/FanClass.py
@@ -1,18 +1,23 @@
 # Create class for fan
 class Fan:
-# Create constannt for fan speed
+# Create constant for fan speed
     SLOW = 1
     MEDIUM = 2
     FAST = 3
 
-# Create constructor
-# Set default fan speed, on, radius and color
-def __init__(self, speed = SLOW, on = False, radius = 5, color = "blue"):
-# Create instance variable
-    self.__speed = speed
-    self.__on = on
-    self.__radius = radius
-    self.__color = color
-    
-# Create getter methods for fan speed, power status, radius, and color
-# Create setter methods for fan speed, power status, radius, and color
+    # Create constructor
+    # Set default fan speed, on, radius and color
+    def __init__(self, speed = SLOW, on = False, radius = 5, color = "blue"):
+    # Create instance variable
+        self.__speed = speed
+        self.__on = on
+        self.__radius = radius
+        self.__color = color
+
+    # Create getter methods for fan speed
+    def get_fan_speed(self):
+        return self.__speed
+        
+    # Create setter methods for fan speed
+    def set_fan_speed(self, speed):
+        self.__speed = speed
