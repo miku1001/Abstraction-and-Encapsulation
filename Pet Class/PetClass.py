804b723f889c3be91438ca68df7fc427f26a7914
miku1001 <pauloted.22@gmail.com>
2023-07-01 18:02:26 +0800

    Pseudocode for PetClass

---
+++ Pet Class/PetClass.py
@@ -0,0 +1,10 @@
+# Create class for pet
+# Create constructor
+# Set default name, animal type, and age
+# Create instance variable
+# Create getter method for name
+# Create setter method for name
+# Create getter method for animal type
+# Create setter method for animal type
+# Create getter method for age
+# Create setter method for age
