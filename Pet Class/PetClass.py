fa1346d015ec8a536df06d86f4212e5c809aaac2
miku1001 <pauloted.22@gmail.com>
2023-07-01 18:03:01 +0800

    Creating class and constructor for PetClass

--- Pet Class/PetClass.py
+++ Pet Class/PetClass.py
@@ -1,10 +1,16 @@
 # Create class for pet
-# Create constructor
-# Set default name, animal type, and age
-# Create instance variable
-# Create getter method for name
-# Create setter method for name
-# Create getter method for animal type
-# Create setter method for animal type
-# Create getter method for age
-# Create setter method for age
+class Pet:
+    # Create constructor
+    # Set default name, animal type, and age
+    def __init__(self, name, animal_type, age):
+        # Create instance variable
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+
+    # Create getter method for name
+    # Create setter method for name
+    # Create getter method for animal type
+    # Create setter method for animal type
+    # Create getter method for age
+    # Create setter method for age
