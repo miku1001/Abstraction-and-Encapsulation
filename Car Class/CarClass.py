81375a035fcfec54fbeafaee4075aa1e357b4e0f
miku1001 <pauloted.22@gmail.com>
2023-07-01 17:23:32 +0800

    Pseudocode for CarClass

---
+++ Car Class/CarClass.py
@@ -0,0 +1,7 @@
+# Create class for fan
+# Create constannt for fan speed
+# Create constructor
+# Set default fan speed, on, radius and color
+# Create instance variable
+# Create getter methods for fan speed, power status, radius, and color
+# Create setter methods for fan speed, power status, radius, and color
